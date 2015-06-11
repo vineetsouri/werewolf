176c9a0f1ed81dfbf66cefa7a219affbeaf51ea9
vineetsouri <vineetsouri72@gmail.com>
2015-06-11 17:06:57 +0530

    changed the code on game.rb,main.rb,player.rb

--- game.rb
+++ game.rb
@@ -1,16 +1,43 @@
 require_relative 'player'
+
 class Game
-  def initialize(no_of_wolves,no_of_villagers)
+  def initialize(no_of_wolves, no_of_villagers)
     @players = []
     no_of_wolves.times do
-      @players.push(player.new(true))
+      @players.push(Player.new(true))
     end
     no_of_villagers.times do
-      @players.push(player.new)
+      @players.push(Player.new)
     end
   end
 
-  def players
-    return @players
+  def play
+    play_night
+  end
+
+private
+
+  def play_night
+    announce "It's night time!"
+    announce "Everybody slept"
+    announce "Wolves wokeup"
+    wolves_kill_villager
+  end
+
+  def announce(str)
+    puts ">> " + str
+    # gets
+  end
+
+  def wolves_kill_villager
+    victim = villagers.sample
+    victim.kill!
+    announce "Wolves killed #{victim.inspect}"
+  end
+
+  def villagers
+    return @players.select do |player|
+      !player.wolf?
+    end
   end
 end
--- player.rb
+++ player.rb
@@ -1,5 +1,14 @@
 class Player
-  def initialize(wolf = false)
-    @wolf? = wolf?
+  def initialize(is_wolf = false)
+    @is_wolf = is_wolf    
+    @alive = true
+  end
+
+  def wolf?
+    @is_wolf
+  end
+
+  def kill!
+    @alive = false
   end
 end
